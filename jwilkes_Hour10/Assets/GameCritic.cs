c9126f0f7d477ad7a302202538e4831bd2aff5d4
John Wilkes <bcmouse@gmail.com>
2024-11-23 13:12:02 -0800

    Project Completed

---
+++ jwilkes_Hour10/Assets/GameCritic.cs
@@ -0,0 +1,22 @@
+/*
+ ________  ________  _____ ______   _______   ________  ________  ___  _________  ___  ________     
+|\   ____\|\   __  \|\   _ \  _   \|\  ___ \ |\   ____\|\   __  \|\  \|\___   ___\\  \|\   ____\    
+\ \  \___|\ \  \|\  \ \  \\\__\ \  \ \   __/|\ \  \___|\ \  \|\  \ \  \|___ \  \_\ \  \ \  \___|    
+ \ \  \  __\ \   __  \ \  \\|__| \  \ \  \_|/_\ \  \    \ \   _  _\ \  \   \ \  \ \ \  \ \  \       
+  \ \  \|\  \ \  \ \  \ \  \    \ \  \ \  \_|\ \ \  \____\ \  \\  \\ \  \   \ \  \ \ \  \ \  \____  
+   \ \_______\ \__\ \__\ \__\    \ \__\ \_______\ \_______\ \__\\ _\\ \__\   \ \__\ \ \__\ \_______\
+    \|_______|\|__|\|__|\|__|     \|__|\|_______|\|_______|\|__|\|__|\|__|    \|__|  \|__|\|_______|
+
+1.  It is challenging, but not to the point where it is frustrating and makes the player want to quit
+2.  Something that may make it more challenging is adding in a couple of obstacles
+3.  Having a graphics such as fireworks when the game is completed would give it a wow factor
+4.  It is fun trying to get the balls into their respective goals.  I think having too many balls on the playfield at one time makes it more tedious
+
+Suggestions:
+
+[x] 1. Making variations of the chaos balls.  Made of different mats and sizes
+[x] 2. Making water sounds when walking
+[x] 3. changing size of bumper
+[x] 4. complex shapes for bumpers
+
+*/
