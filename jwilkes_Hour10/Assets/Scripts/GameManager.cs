82a2031fbffcd9551ab879a1e82dd48c67053a5b
John Wilkes <Mousserr@users.noreply.github.com>
2024-11-23 12:12:59 -0800

    Initial upload
    Program complete.  Need to finish questions and 4 items

---
+++ jwilkes_Hour10/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GoalScript blue, green, red, orange;
+	private bool isGameOver = true;
+
+	
+	// Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		// If all four goals are solved then the game is over
+		isGameOver = blue.isSolved && green.isSolved && red.isSolved && orange.isSolved;
+    }
+
+	private void OnGUI()
+	{
+		if (isGameOver)
+		{
+			Rect rect = new Rect (Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 75);		//Set gui to middle of screen
+			GUI.Box (rect, "Game Over");															//Puts "Game Over" in center of screen
+			
+			Rect rect2 = new Rect (Screen.width /2 - 30, Screen.height / 2 - 25, 60, 50);
+			GUI.Box (rect2, "Good Job!");
+		}
+	}
+}
