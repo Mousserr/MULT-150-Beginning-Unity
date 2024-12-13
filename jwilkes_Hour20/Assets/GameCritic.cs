6b318c6f163f76d494f9b939c75a5f5bdd74ae44
John Wilkes <Mousserr@users.noreply.github.com>
2024-12-13 14:45:22 -0800

    Final project

---
+++ jwilkes_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,31 @@
+/*
+
+ ,ad8888ba,                                             ,ad8888ba,             88         88             
+ d8"'    `"8b                                           d8"'    `"8b            ""   ,d    ""             
+d8'                                                    d8'                           88                   
+88            ,adPPYYba, 88,dPYba,,adPYba,   ,adPPYba, 88            8b,dPPYba, 88 MM88MMM 88  ,adPPYba,  
+88      88888 ""     `Y8 88P'   "88"    "8a a8P_____88 88            88P'   "Y8 88   88    88 a8"     ""  
+Y8,        88 ,adPPPPP88 88      88      88 8PP""""""" Y8,           88         88   88    88 8b          
+ Y8a.    .a88 88,    ,88 88      88      88 "8b,   ,aa  Y8a.    .a8P 88         88   88,   88 "8a,   ,aa  
+  `"Y88888P"  `"8bbdP"Y8 88      88      88  `"Ybbd8"'   `"Y8888Y"'  88         88   "Y888 88  `"Ybbd8"'  
+
+1 - a. Too easy.  The pattern is predicatable between squares and balls.  There should be random spawing of the squares and balls in various orders
+	b. Too easy.  The spawn times are very predicatable.  They should spawn at a random time vs. at the consistent spawn every 5 seconds.
+
+2 - a. To make harder - Add code to make squares move back and forth at a random speed / distance.
+	b. To make harder - Change the ratio of powerups vs. squares
+
+3 - a. Add sound effects.
+	b. Make powerups explode with effects
+
+4 - a. Trying to get to a certain time goal
+	b. Being able to dodge squares
+
+5 - a. Moving left and right at slower speed.  Hard to go between blocks and balls in most cases
+	b. Slowing down when hit square.  It throws the rythm off.  There should be a time penalty, not slow penalty.
+
+Changes:
+Obstacles - Changed light from red to blue
+Particles - Added change color over time
+
+*/
